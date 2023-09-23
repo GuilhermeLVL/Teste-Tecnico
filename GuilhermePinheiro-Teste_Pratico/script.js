a22c9e330713e21412fd6987a56b0c3a070475a0
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-23 19:15:45 -0300

    s

--- GuilhermePinheiro-Teste_Pratico/script.js
+++ GuilhermePinheiro-Teste_Pratico/script.js
@@ -1,79 +1,135 @@
-function loadData(category) {
-    $.ajax({
-      url: `https://swapi.dev/api/${category}/`,
-      method: 'GET',
-      success: function(data) {
-        if (category === 'films') {
-          displayFilms(data.results);
-        } else {
-          displayData(data.results, category);
-        }
-      },
-      error: function(error) {
-        console.error(`Erro ao obter os dados da categoria ${category} da API SWAPI:`, error);
-      }
-    });
-  }
-  
-  function displayData(data, category) {
-    const dataContainer = document.getElementById('dados');
-    dataContainer.innerHTML = '';  // Limpa o conteúdo anterior
-  
-    if (data.length === 0) {
-      dataContainer.innerHTML = `<p>Nenhum dado encontrado para a categoria ${category}.</p>`;
-      return;
-    }
-  
-    data.forEach(item => {
-      const itemElement = document.createElement('div');
-      itemElement.classList.add('item');
-  
-      let displayText = '';
-  
-      if (category === 'people') {
-        displayText = `<strong>Name:</strong> ${item.name}, <strong>Gender:</strong> ${item.gender}, <strong>Height:</strong> ${item.height}`;
-      } else if (category === 'planets') {
-        displayText = `<strong>Name:</strong> ${item.name}, <strong>Climate:</strong> ${item.climate}, <strong>Population:</strong> ${item.population}`;
-      } else if (category === 'species') {
-        displayText = `<strong>Name:</strong> ${item.name}, <strong>Classification:</strong> ${item.classification}, <strong>Language:</strong> ${item.language}`;
-      } else if (category === 'starships') {
-        displayText = `<strong>Name:</strong> ${item.name}, <strong>Model:</strong> ${item.model}, <strong>Manufacturer:</strong> ${item.manufacturer}`;
-      } else if (category === 'vehicles') {
-        displayText = `<strong>Name:</strong> ${item.name}, <strong>Model:</strong> ${item.model}, <strong>Manufacturer:</strong> ${item.manufacturer}`;
-      }
-  
-      itemElement.innerHTML = `
-  <h2>${item.name.charAt(0).toUpperCase() + item.name.slice(1)}</h2>
-        ${displayText}
-      `;
-  
-      dataContainer.appendChild(itemElement);
-    });
-  }
-  
-  function displayFilms(films) {
-    const dataContainer = document.getElementById('dados');
-    dataContainer.innerHTML = '';  // Limpa o conteúdo anterior
-  
-    if (films.length === 0) {
-      dataContainer.innerHTML = '<p>Nenhum filme encontrado.</p>';
-      return;
-    }
-  
-    films.forEach(film => {
-      const filmElement = document.createElement('div');
-      filmElement.classList.add('item');
-      filmElement.innerHTML = `
+let currentPage = 1;
+let currentCategory = '';
+let maxItemsToShow = 6;  // Defina o número máximo de itens a serem exibidos
 
-        <h2>${film.title}</h2>
-        <strong>Title:</strong> ${film.title}<br>
-        <strong>Episode:</strong> ${film.episode_id}<br>
-        <strong>Director:</strong> ${film.director}<br>
-        <strong>Producer:</strong> ${film.producer}<br>
-        <strong>Release Date:</strong> ${film.release_date}
-      `;
-  
-      dataContainer.appendChild(filmElement);
-    });
+function loadData(category) {
+  currentCategory = category;
+  currentPage = 1;
+  fetchData(category, currentPage);
+}
+
+function loadNextPage() {
+  currentPage++;
+  fetchData(currentCategory, currentPage);
+}
+
+function loadPreviousPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchData(currentCategory, currentPage);
   }
-  
+}
+
+function fetchData(category, page) {
+  $.ajax({
+    url: `https://swapi.dev/api/${category}/?page=${page}`,
+    method: 'GET',
+    success: function(data) {
+      displayData(data.results, category);
+    },
+    error: function(error) {
+      console.error(`Erro ao obter os dados da categoria ${category} da API SWAPI:`, error);
+    }
+  });
+}
+
+function displayData(data, category) {
+  const dataContainer = document.getElementById('dados');
+  dataContainer.innerHTML = '';
+
+  if (data.length === 0) {
+    dataContainer.innerHTML = `<p>Nenhum dado encontrado para a categoria ${category}.</p>`;
+    return;
+  }
+
+  // Limita a exibição apenas à quantidade máxima de itens
+  const limitedData = data.slice(0, maxItemsToShow);
+
+  limitedData.forEach((item, index) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.classList.add('my-3');
+    const collapseId = `${category}-collapse-${index}`;
+    
+    if (category === 'films') {
+      card.innerHTML = `
+        <div class="card-header" id="heading${collapseId}">
+          <h5 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${collapseId}" aria-expanded="false" aria-controls="${collapseId}">
+              ${item.title.charAt(0).toUpperCase() + item.title.slice(1)}
+            </button>
+          </h5>
+        </div>
+
+        <div id="${collapseId}" class="collapse" aria-labelledby="heading${collapseId}" data-parent="#dados">
+          <div class="card-body">
+            ${generateCardContent(item, category, collapseId)}
+          </div>
+        </div>
+      `;
+    } else {
+      card.innerHTML = `
+        <div class="card-header" id="heading${collapseId}">
+          <h5 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${collapseId}" aria-expanded="false" aria-controls="${collapseId}">
+              ${item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+            </button>
+          </h5>
+        </div>
+
+        <div id="${collapseId}" class="collapse" aria-labelledby="heading${collapseId}" data-parent="#dados">
+          <div class="card-body">
+            ${generateCardContent(item, category, collapseId)}
+          </div>
+        </div>
+      `;
+    }
+
+    dataContainer.appendChild(card);
+  });
+}
+
+function generateCardContent(item, category, collapseId) {
+  let content = '';
+
+  if (category === 'people') {
+    content = `
+    <strong>Name:</strong> ${item.name}<br>
+    <strong>Gender:</strong> ${item.gender}<br> 
+    <strong>Height:</strong> ${item.height}<br>
+    <strong>Mass:</strong> ${item.mass}<br>
+    <strong>Birth Year:</strong> ${item.birth_year}<br>
+    `;
+  } else if (category === 'planets') {
+    content = `<strong>Name:</strong> ${item.name}<br> 
+    <strong>Climate:</strong> ${item.climate}<br> 
+    <strong>Population:</strong> ${item.population}<br>
+    <strong>Diameter:</strong> ${item.diameter}<br>
+    <strong>Terrain:</strong> ${item.terrain}<br>
+    `;
+  } else if (category === 'species') {
+    content = `
+    <strong>Name:</strong> ${item.name}<br>       
+    <strong>Classification:</strong> ${item.classification}<br> 
+    <strong>Language:</strong> ${item.language}<br>
+    <strong>Average Lifespan:</strong> ${item.average_lifespan}<br>
+    <strong>Average Height:</strong> ${item.average_height}<br>
+    `;
+  } else if (category === 'starships' || category === 'vehicles') {
+    content = `<strong>Name:</strong> ${item.name}<br>
+    <strong>Model:</strong> ${item.model}<br>
+    <strong>Manufacturer:</strong> ${item.manufacturer}<br>           
+    <strong>Cargo Capacity:</strong> ${item.cargo_capacity}<br>
+    `;
+  } else if (category === 'films') {
+    content = `
+      <strong>Title:</strong> ${item.title}<br>
+      <strong>Episode:</strong> ${item.episode_id}<br>
+      <strong>Director:</strong> ${item.director}<br>
+      <strong>Producer:</strong> ${item.producer}<br>
+      <strong>Release Date:</strong> ${item.release_date}
+    `;
+  }
+
+  return content;
+}
