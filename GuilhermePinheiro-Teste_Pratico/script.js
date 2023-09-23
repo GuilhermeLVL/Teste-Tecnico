96456e75c52b537f8bdebc648d38ae2bcd3a0c59
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-09-23 12:59:30 -0300

    s

--- GuilhermePinheiro-Teste_Pratico/script.js
+++ GuilhermePinheiro-Teste_Pratico/script.js
@@ -1,37 +1,55 @@
-$(document).ready(function() {
+//Passando o valor de forma dinâmica para coletar os dados da api
+function loadData(category) {
     $.ajax({
-      url: 'https://swapi.dev/api/people/',
+      url: `https://swapi.dev/api/${category}/`,
       method: 'GET',
       success: function(data) {
-        displayData(data.results);
+        displayData(data.results, category);
       },
       error: function(error) {
-        console.error('Erro ao obter os dados da API SWAPI:', error);
+        console.error(`Erro ao obter os dados da categoria ${category} da API SWAPI:`, error);
       }
     });
-  });
+  }
   
-  function displayData(data) {
+  //Pegando a DIV pelo Id e Limpando o conteudo
+  function displayData(data, category) {
     const dataContainer = document.getElementById('dados');
-    dataContainer.innerHTML = '';  // Limpa o conteúdo anterior
+    dataContainer.innerHTML = ''; 
   
+    //Verificando se há dados 
     if (data.length === 0) {
-      dataContainer.innerHTML = '<p>Nenhum dado encontrado.</p>';
+      dataContainer.innerHTML = `<p>Nenhum dado encontrado para a categoria ${category}.</p>`;
       return;
     }
   
     data.forEach(item => {
       const itemElement = document.createElement('div');
       itemElement.classList.add('item');
+  
+      let displayText = '';
+  
+      // Exibe informações específicas para cada categoria
+      if (category === 'people') {
+        displayText = `<strong>Name:</strong> ${item.name}, <strong>Gender:</strong> ${item.gender}, <strong>Height:</strong> ${item.height}`;
+      } else if (category === 'films') {
+        displayText = `<strong>Title:</strong> ${item.title}, <strong>Episode:</strong> ${item.episode_id}, <strong>Director:</strong> ${item.director}`;
+      } else if (category === 'planets') {
+        displayText = `<strong>Name:</strong> ${item.name}, <strong>Climate:</strong> ${item.climate}, <strong>Population:</strong> ${item.population}`;
+      } else if (category === 'species') {
+        displayText = `<strong>Name:</strong> ${item.name}, <strong>Classification:</strong> ${item.classification}, <strong>Language:</strong> ${item.language}`;
+      } else if (category === 'starships') {
+        displayText = `<strong>Name:</strong> ${item.name}, <strong>Model:</strong> ${item.model}, <strong>Manufacturer:</strong> ${item.manufacturer}`;
+      } else if (category === 'vehicles') {
+        displayText = `<strong>Name:</strong> ${item.name}, <strong>Model:</strong> ${item.model}, <strong>Manufacturer:</strong> ${item.manufacturer}`;
+      }
+  
       itemElement.innerHTML = `
-        <h2>${item.name}</h2>
-        <p>Gênero: ${item.gender}</p>
-        <p>Altura: ${item.height}</p>
-        <p>Peso: ${item.mass}</p>
-        <p>Cor dos olhos: ${item.eye_color}</p>
-        <p>Cor do cabelo: ${item.hair_color}</p>
+        <h2>${item.name.charAt(0).toUpperCase() + item.name.slice(1)}</h2>
+        ${displayText}
       `;
   
       dataContainer.appendChild(itemElement);
     });
   }
+  
